6c1e2d9294577143bc04fa49a5d67a3fe8d4e902
Aashik-Roy001 <aashikroy01@gmail.com>
2024-05-21 12:27:27 +0530

    url-shortner

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require("express");
 
 const app = express();
+const path = require("path");
 const port = 8001;
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/urlRouter");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./model/url");
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => {
   console.log("connected to MongoDB");
 });
 
-app.use(express.json());
+// ========== set the view Engine =========================
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
+// =========== usign the middleware =======================
+app.use(express.json()); // for json data
+app.use(express.urlencoded({ extended: false })); // to parse the form data
+// ============== using the routes ========================
 app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRoute);
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     { shortId },
